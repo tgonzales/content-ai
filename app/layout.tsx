0f0f13e18964a0975d58c206cbd9235186f7292a
cleiton couto <dev.cleiton.couto+capim@gmail.com>
2024-09-05 21:00:59 -0300

    feat: remove header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,11 @@
 import "@/styles/globals.css"
 import { Metadata } from "next"
+import { JetBrains_Mono as FontMono, Inter as FontSans } from "next/font/google"
 
 import { siteConfig } from "@/config/site"
-
+import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils"
-
-import { JetBrains_Mono as FontMono, Inter as FontSans } from "next/font/google"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -53,7 +51,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
         >
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <div className="relative flex flex-col min-h-screen">
-              <SiteHeader />
               <div className="flex-1">{children}</div>
             </div>
           </ThemeProvider>
